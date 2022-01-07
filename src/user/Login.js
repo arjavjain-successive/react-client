0edcd8951759abd3e2958df6fcbe9b317e74779c
Arjav jain <arjav.jain@arjavr020.successivetech.com>
2022-01-07 14:11:04 +0530

    class component

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './AppCss.css'
 import { TextField,Button,Form,FormLayout} from '@shopify/polaris';
-import { useState } from 'react';
-const  Login = () => {
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const onSubmitt = () =>{
-    window.alert("Email:"+ email + "  Password:" + password);
-    setEmail('');
-    setPassword('');
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.onSubmitt = this.onSubmitt.bind(this);
   }
+  onSubmitt(event) {
+    window.alert("email:" + this.state.email)
+    this.setState({email:"",password:""});
+    event.preventDefault();
+  }
+  render(){
   return( 
     <div className = "form">
     <h1 className = "heading">Welcome to my Login Page</h1>
-      <Form onSubmit = {onSubmitt}> 
+      <Form onSubmit = {this.onSubmitt}> 
           <FormLayout>
             <div className = "textfield">
-     <TextField label="Email" autoComplete="off" align="left" type="email" value= {email}
-      onChange ={ (newValue) => setEmail(newValue)} />
+     <TextField label="Email" autoComplete="off" align="left" type="email" value= {this.state.email}
+      onChange = {(newValue) => this.setState({email:newValue})} />
             <br/>
-            <TextField label="Password" autoComplete="off" align="left" type="password" value = {password}
-      onChange ={ (newValue) => setPassword(newValue)} />
+            <TextField label="Password" autoComplete="off" align="left" type="password" value = {this.state.password}
+     onChange = {(newValue) => this.setState({password:newValue})} />
       </div>
       <div className="submit">
             <Button submit >Submit</Button>
@@ -29,6 +35,6 @@ const  Login = () => {
         </Form>
       </div>
   );
-
+  }
 }
 export default Login;
