be05796f9c3c133442390e5b0fd47097f7d0d095
Arjav jain <arjav.jain@arjavr020.successivetech.com>
2022-01-31 15:34:35 +0530

    Task completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import enTranslations from '@shopify/polaris/locales/en.json';
 import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
-import Routers from './route/Routes';
+import Routers from './route/route';
 const App = () => {
   return (
     <div>
